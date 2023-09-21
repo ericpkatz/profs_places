b3b3d3ae4abd9b4b4fa15dbaa14913c9d61b0d42
ericpkatz <ericpkatz@gmail.com>
2023-09-21 13:05:07 -0400

    commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const pg = require('pg');
-const client = new pg.Client('postgres://localhost/fullstack_template_db');
+const client = new pg.Client('postgres://localhost/profs_places_db');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -16,12 +16,35 @@ app.get('/dist/main.js.map', (req, res)=> res.sendFile(reactSourceMap));
 const styleSheet = path.join(__dirname, 'styles.css');
 app.get('/styles.css', (req, res)=> res.sendFile(styleSheet));
 
+app.get('/api/places', async(req, res, next)=> {
+  try {
+    const SQL = `
+      SELECT *
+      FROM places
+    `;
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
 const init = async()=> {
   await client.connect();
   console.log('connected to database');
   const SQL = `
-    SQL SETUP AND SEED
+    DROP TABLE IF EXISTS places;
+    CREATE TABLE places(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100)
+    );
+    INSERT INTO places(name) VALUES('Mount Vernon');
+    INSERT INTO places(name) VALUES('NYC');
+    INSERT INTO places(name) VALUES('LA');
   `;
+  await client.query(SQL);
+
   console.log('create your tables and seed data');
 
   const port = process.env.PORT || 3000;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 
 const App = ()=> {
+  const [places, setPlaces] = useState([]);
+  useEffect(()=> {
+    const fetchPlaces = async()=> {
+      const response = await axios.get('/api/places');
+      setPlaces(response.data);
+    };
+
+    fetchPlaces();
+  },  []);
   return (
-    <h1>FullStack Template</h1>
+    <div>
+      <h1>Profs Places ({ places.length })</h1>
+      <ul>
+        {
+          places.map( place => {
+            return (
+              <li key={ place.id }>
+                { place.name }
+              </li>
+            );
+          })
+        }
+      </ul>
+    </div>
   );
 };
 
